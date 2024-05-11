9a5a918532e4715ea782c474973d722e4be7e241
aron edwards <aronedwards91@gmail.com>
2024-05-11 12:19:57 +0100

    resizable sidebar

---
+++ src/components/button.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let onclick;
+  export let className;
+  export let active = false;
+</script>
+
+<button
+  on:click={onclick}
+  class={`p-1.5 rounded-md bg-slate-100 border border-black ${className} ${active ? "bg-slate-400" : ""}`}
+  ><slot /></button
+>
--- src/components/sidebar.svelte
+++ src/components/sidebar.svelte
@@ -1,10 +1,58 @@
 <script>
+  import Icon from "@iconify/svelte";
+  import Button from "./button.svelte";
   export let config = {};
+
+  const options = {
+    thin: "thin",
+    mid: "mid",
+    wide: "wide",
+  };
+  let currentWidth = options.mid;
 </script>
 
-<div class="w-32 h-vscreen flex flex-col bg-slate-500">
+<div
+  class={`type-${currentWidth} w-32 h-vscreen flex flex-col bg-slate-500 border-r border-black`}
+>
+  <div class="flex justify-between p-1.5">
+    <Button
+      onclick={() => (currentWidth = options.thin)}
+      className={currentWidth === options.thin ? "hidden" : ""}
+    >
+      <Icon class="rotate-180" icon="fluent:arrow-next-12-filled" /></Button
+    >
+    <Button
+      onclick={() => (currentWidth = options.mid)}
+      className={currentWidth === options.thin ? "hidden" : ""}
+      active={currentWidth === options.mid}
+      ><Icon icon="fluent:arrow-left-32-filled" /></Button
+    >
+    <Button
+      onclick={() => (currentWidth = options.wide)}
+      className={currentWidth === options.thin ? "absolute z-20" : ""}
+      active={currentWidth === options.wide}
+    >
+      <Icon icon="fluent:arrow-next-12-filled" />
+    </Button>
+  </div>
+
   <h2>Sidebar</h2>
   <!-- line,small,max -->
 
   <div>{config.setup.points[0].startValue}</div>
 </div>
+
+<style>
+  div.type-wide {
+    position: absolute;
+    width: 80%;
+    max-width: 36rem;
+    height: 100vh;
+  }
+  div.type-mid {
+    width: 16rem;
+  }
+  div.type-thin {
+    width: 1px;
+  }
+</style>
